74d16d5fa0663a81e9d470c46f25a94cae2c1f51
alexandre <alexandre@squadracer.com>
2023-09-26 09:07:55 +0200

    Update 2023-09-26

--- script/history/fix_prev_next.rb
+++ script/history/fix_prev_next.rb
@@ -1,7 +1,7 @@
 
 Dir::glob("docs/events/*").unshift(nil).push(nil).each_cons(3) do |a, b, c|
   IO.write(b,
-    IO.read(b).sub(/(<a href="\/events\/)[\d-]+?(\.html"\>prev day<\/a>)/, a ? "\\1#{a[/[\d-]+/]}\\2" : "")
-              .sub(/(<a href="\/events\/)[\d-]+?(\.html"\>next day<\/a>)/, c ? "\\1#{c[/[\d-]+/]}\\2" : "")
+    IO.read(b).sub(/(<a href="\/events\/)([\d-]+?)\.html".*?(>prev day<\/a>)/){ d = $3; "#$1#{a ? a[/[\d-]+/] : $2}.html\"#{a ? "" : 'style="display:none"'}#{d}" }
+              .sub(/(<a href="\/events\/)([\d-]+?)\.html".*?(>next day<\/a>)/){ d = $3; "#$1#{c ? c[/[\d-]+/] : $2}.html\"#{c ? "" : 'style="display:none"'}#{d}" }
   )
 end

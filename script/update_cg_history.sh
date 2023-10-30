367d284626b8aa5534db786df7c7ea5a2a78dbf4
alexandre <alexandre@squadracer.com>
2023-10-30 09:44:53 +0100

    sh compliant loop

--- script/update_cg_history.sh
+++ script/update_cg_history.sh
@@ -2,7 +2,8 @@
 rm -rf tmp/puzzle
 
 # Get sometimes a 502 Bad Gateway... so retry!
-for retry in {1..20}; do
+#for retry in {1..20}; do
+for ((retry = 1; retry <= 20; retry++)); do
   echo try $retry
   git restore puzzle
   ruby script/update_docs.rb && {
